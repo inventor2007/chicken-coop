2a6d2dd449d357f8c252de2893053e7f739be678
Emilien B <66035084+inventor2007@users.noreply.github.com>
2025-03-27 16:50:00 +0000

    Activation de CORS pour toutes les routes de l'API

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 import time
 from gpiozero import LightSensor, Button, Servo
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Activer CORS sur toutes les routes
 
 ldr = LightSensor(17)
 flotteur = Button(19)
